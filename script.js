64208eea239f1d75dbbcd85bf26c9618a397ba79
Chen Wei Lun <weilunchen210@gmail.com>
2025-07-18 16:21:15 +0800

    main function done, awaiting game loop

--- script.js
+++ script.js
@@ -7,14 +7,32 @@ function addCounter(){
     divCount++;
     const counterContainer = document.createElement("div")
     counterContainer.id = `div${divCount}`
-    counterContainer.innerHTML = `<div class="counter-container">
-                    <div class="container-actions">
-                        <img onclick=remove(this) src="https://img.icons8.com/?size=100&id=11705&format=png&color=FFFFFF" class="icon"/>
-                    </div>
-                    <h3>Counter 1</h3>
-                    <h1 class="count">0</h1>
-                    <button onClick=incrementCount(this)>Click</button>
-                </div>`
+    // counterContainer.innerHTML = `<div class="counter-container-grid"> 
+    //     <div class="counter-container">
+    //                 <div class="container-actions">
+    //                     <img onclick=remove(this) src="https://img.icons8.com/?size=100&id=11705&format=png&color=FFFFFF" class="icon"/>
+    //                 </div>
+    //                 <h3>Counter 1</h3>
+    //                 <h1 class="count">0</h1>
+    //                 <button onClick=incrementCount(this)>Click</button>
+    //             </div>
+    // </div>`
+
+    counterContainer.innerHTML = `
+        <div class="counter-container-grid">
+            <div class ="counter-container"  onClick="handleClick(this)">
+
+            </div>
+        </div>
+    `
+    const rand = Math.floor(Math.random(100)*100) + 5;
+    const height= rand + "%"
+    const width = rand +"%"
+    //counterContainer.querySelector('.count').innerText = rand
+    const parentDiv = counterContainer.querySelector('.counter-container')
+    parentDiv.style.height = height
+    parentDiv.style.width = width
+
 
     main.appendChild(counterContainer)
 }
@@ -26,9 +44,36 @@ function remove(img){
 
 }
 
-function incrementCount(button){
-    const counter = button.closest('.counter-container').querySelector('.count')
-    let currentCount = parseInt(counter.innerText)
-    currentCount++
-    counter.innerText=currentCount
+function handleClick(block){
+    let number = parseInt(block.style.height);
+    console.log(number)
+    number = number - 5
+    if(number < 10){
+        number = 0
+        const parentDiv = block.parentElement
+        const grandparentDiv = parentDiv.parentElement
+        grandparentDiv.remove()
+        
+    }
+    block.style.height=number + "%";
+    block.style.width=number + "%";
+}
+
+function startTimer(button){
+
+    const display = button.closest("header").querySelector(".timer")
+    var timer = 60 , minutes, seconds
+     setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = 60;
+        }
+    }, 1000);
 }
